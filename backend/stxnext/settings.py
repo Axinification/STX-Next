49ca666857ddce0ddcd74f9d418da2c6e85eea3d
Aleksander Witocha <alekwit@gmail.com>
2022-05-16 23:47:56 +0200

    xd

--- backend/stxnext/settings.py
+++ backend/stxnext/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from sys import os
+from os import path
 import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,7 +134,7 @@ USE_TZ = True
 
 # STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'statictifies')
+STATIC_ROOT = path.join(BASE_DIR, 'statictifies')
 STATIC_URL = "/static/"
 django_heroku.settings(locals())
 
